3ad171840f7ddfc6ad442cb187755e91f63dbc9c
segalzeyalz <segaleseo@gmail.com>
2018-11-26 17:29:29 +0200

    add buttons logics - hide and show according to a logics

--- xioma-test/src/buttons/Buttons.js
+++ xioma-test/src/buttons/Buttons.js
@@ -4,26 +4,18 @@ class Buttons extends Component {
   constructor(props){
     super(props);
       this.state = {
+        logged: true
     }
-    this.deleteShape = this.deleteShape.bind(this)
   }
-
-  deleteShape(){
-    let { shapes } = this.state;
-    shapes.splice(shapes.length-1,1)
-    this.setState({shapes})
-  }
-
-
   render() {
       //Here is the Buttons
     return (
       <div className="buttons">
         <div className="button">Sign In</div>
         <div className="button">Sign Up</div>
-        {<div className="button">Add New Business Partner</div>}
-        <div className="button">Show All </div>
-      </div>
+        {this.state.logged && <div> <div className="button">Add New Business Partner</div>
+        <div className="button">Show All </div></div>} 
+        </div>
     );
   }
 }
