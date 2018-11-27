0556c8a8377b07eb10b9a9334886c1b2b147cadc
segalzeyalz <segaleseo@gmail.com>
2018-11-27 17:20:24 +0200

    send something to the comp

--- xioma-test/src/App.js
+++ xioma-test/src/App.js
@@ -1,14 +1,29 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 import Buttons from './buttons/Buttons'
 import Businessparetners from './businesspartners'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        logged: false,
+        businessparetners:["lalala", "momo"]
+    }
+    this.login = this.login.bind(this)
+    this.signOut = this.signOut.bind(this)
+  }
+  login(){
+    this.setState({logged:true})
+  }
+  signOut(){
+    this.setState({logged:false})
+  }
   render() {
     return (
       <div className="App">
-        <Buttons />
-        <Businessparetners businessparetners={"lalala"}/>
+        <Buttons login={this.login} signOut={this.signOut} logged={this.state.logged}/>
+        <Businessparetners businessparetners={this.state.businessparetners}/>
       </div>
     );
   }
--- xioma-test/src/buttons/Buttons.js
+++ xioma-test/src/buttons/Buttons.js
@@ -4,25 +4,17 @@ class Buttons extends Component {
   constructor(props){
     super(props);
       this.state = {
-        logged: false
     }
-    this.login = this.login.bind(this)
-    this.signOut = this.signOut.bind(this)
   }
-  login(){
-    this.setState({logged:true})
-  }
-  signOut(){
-    this.setState({logged:false})
-  }
+  
   render() {
       //Here is the Buttons
     return (
       <div className="buttons">
-        <div className="button" onClick={this.login}>Sign In</div>
-        <div className="button" onClick={this.signOut}>Sign Out</div>
+        <div className="button" onClick={this.props.login}>Sign In</div>
+        <div className="button" onClick={this.props.signOut}>Sign Out</div>
         <div className="button">Sign Up</div>
-        {this.state.logged && <div> <div className="button">Add New Business Partner</div>
+        {this.props.logged && <div> <div className="button">Add New Business Partner</div>
         <div className="button">Show All </div></div>} 
         </div>
     );
